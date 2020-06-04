eb880653c4ba0ad85e6356af49edcf0648187b8e
Piers Pollard <piers@madebyprism.com>
2020-06-04 11:12:43 +0100

    fix: Ts export

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import { useToast, ToastContextProps } from './components/ToastContext/ToastContext';
+import { useToast, ToastContextProps, ToastProps } from './components/ToastContext/ToastContext';
 import ToastProvider from './components/ToastProvider/ToastProvider';
 
 export {
   useToast,
   ToastProvider,
   ToastContextProps,
+  ToastProps,
 };
